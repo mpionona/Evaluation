69d9922260c5ebe6298d8a11fe1dce7f66e622a7
Mpionona <mpionnhera@gmail.com>
2024-08-30 10:16:55 +0200

    Mettre en marche les selction Autre

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,22 @@
 function checkAutre(select) {
-    // Détermine l'ID de la div associée en fonction de l'ID du select
-    var divId;
-    
+    // Récupère toutes les divs liées au formulaire spécifique
+    const relatedDivs = select.parentElement.parentElement.querySelectorAll("div[id^='Autre']");
+
+    // Masque uniquement les divs liées à l'élément select
+    relatedDivs.forEach(div => {
+        div.style.display = "none";
+    });
+
+    // Affiche la bonne div en fonction du select et de la valeur choisie
     if (select.id === "activite" && select.value === "Autre") {
-        divId = "autreActiviteDiv";
+        document.getElementById("autreActiviteDiv").style.display = "block";
     } else if (select.id === "profil" && select.value === "AutreProfil") {
-        divId = "autreProfilDiv";
-    }
-    
-    // Masque les divs par défaut
-    document.getElementById("autreActiviteDiv").style.display = "none";
-    document.getElementById("autreProfilDiv").style.display = "none";
-    
-    // Affiche la div correspondante
-    if (divId) {
-        document.getElementById(divId).style.display = "block";
+        document.getElementById("autreProfilDiv").style.display = "block";
+    } else if (select.id === "education" && select.value === "AutreProfilEducation") {
+        document.getElementById("AutreProfilEducationDiv").style.display = "block";
+    } else if (select.id === "sante" && select.value === "AutreProfilSante") {
+        document.getElementById("AutreProfilSanteDiv").style.display = "block";
+    } else if (select.id === "motif" && select.value === "AutreMotif") {
+        document.getElementById("AutreMotifDiv").style.display = "block";
     }
 }
--- resources/views/formulaire.blade.php
+++ resources/views/formulaire.blade.php
@@ -63,10 +63,10 @@
     </form>
     <h5>1.4 Secteur d'activités</h5>
     <form action="">
-    <!-- Liste déroulante -->
+    <!-- Liste déroulante Secteur d'activités-->
         <div>
             <select id="activite" name="activite" onchange="checkAutre(this)">
-                <option value=""><em>Sélectionnez une activité</em></option>
+                <option value="" disabled selected hidden>Sélectionnez une activité</option>
                 <option value="Agriculture">Agriculture</option>
                 <option value="Industrie">Industrie</option>
                 <option value="Construction">Construction</option>
@@ -89,48 +89,170 @@
             <label for="autreActivite">Veuillez préciser votre secteur d'activité:</label>
             <input type="text" id="autreActivite" name="autreActivite">
         </div>
-</form>
+    </form>
 
-<h6>1.4.1 Education</h6>
+    <h6>1.4.1 Profil dans l'éducation</h6>
     <form action="">
-    <!-- Liste déroulante -->
+    <!-- Liste déroulante Profil dans l'éducation-->
         <div>
-            <select id="activite" name="activite" onchange="checkAutre(this)">
-                <option value=""><em>Sélectionnez une activité</em></option>
-                <option value="Agriculture">Etudiant</option>
-                <option value="Industrie">Chercheur</option>
-                <option value="Construction">Professeur</option>
-                <option value="Commerce">Autre</option>
+            <select id="education" name="education" onchange="checkAutre(this)">
+                <option value="" disabled selected hidden>Sélectionnez un profil</option>
+                <option value="Etudiant">Etudiant</option>
+                <option value="Chercheur">Chercheur</option>
+                <option value="Professeur">Professeur</option>
+                <option value="AutreProfilEducation">Autre</option>
             </select>
         </div>
 
         <!-- Zone de texte cachée par défaut -->
-        <div id="autreActiviteDiv" style="display: none;">
-            <label for="autreActivite">Veuillez préciser votre secteur d'activité:</label>
-            <input type="text" id="autreActivite" name="autreActivite">
+        <div id="AutreProfilEducationDiv" style="display: none;">
+            <label for="AutreProfilEducation">Veuillez préciser votre secteur d'activité:</label>
+            <input type="text" id="AutreProfilEducation" name="AutreProfilEducation">
+        </div>
+    </form>
+    <h6>1.4.1 Profils dans le secteur médical</h6>
+    <form action="">
+    <!-- Liste déroulante Profil dans la santé-->
+        <div>
+            <select id="sante" name="sante" onchange="checkAutre(this)">
+                <option value="" disabled selected hidden>Sélectionnez un profil</option>
+                <option value="Responsable">Responsable</option>
+                <option value="Docteur">Docteur</option>
+                <option value="Sagefemme">Sagefemme</option>
+                <option value="Aide">Aide soignante</option>
+                <option value="AutreProfilSante">Autre</option>
+            </select>
+        </div>
+
+        <!-- Zone de texte cachée par défaut -->
+        <div id="AutreProfilSanteDiv" style="display: none;">
+            <label for="AutreProfilSante">Veuillez préciser votre secteur d'activité:</label>
+            <input type="text" id="AutreProfilSante" name="AutreProfilSante">
         </div>
     </form>
 
-<h5>1.5 Catégories de profils professionnels</h5>
-<form action="">
-    <!-- Liste déroulante pour les profils professionnels -->
-    <div>
-        <select id="profil" name="profil" onchange="checkAutre(this)">
-            <option value=""><em>Sélectionnez un profil</em></option>
-            <option value="Etudiant">Étudiant</option>
-            <option value="Decideur">Décideur</option>
-            <option value="Entrepreneur">Entrepreneur</option>
-            <option value="AutreProfil">Autre</option>
-        </select>
-    </div>
+    <h5>1.5 Catégories de profils professionnels</h5>
+    <form action="">
+        <!-- Liste déroulante pour les profils professionnels -->
+        <div>
+            <select id="profil" name="profil" onchange="checkAutre(this)">
+                <option value="" disabled selected hidden>Sélectionnez un profil</option>
+                <option value="Etudiant">Étudiant</option>
+                <option value="Decideur">Décideur</option>
+                <option value="Entrepreneur">Entrepreneur</option>
+                <option value="AutreProfil">Autre</option>
+            </select>
+        </div>
 
-    <!-- Zone de texte pour autre profil professionnel -->
-    <div id="autreProfilDiv" style="display: none;">
-        <label for="autreProfil">Veuillez préciser votre profil professionnel:</label>
-        <input type="text" id="autreProfil" name="autreProfil">
-    </div>
-</form>
+        <!-- Zone de texte pour autre profil professionnel -->
+        <div id="autreProfilDiv" style="display: none;">
+            <label for="autreProfil">Veuillez préciser votre profil professionnel:</label>
+            <input type="text" id="autreProfil" name="autreProfil">
+        </div>
+    </form>
+    <h2>2. Utilisation du site</h2>
+    <h5>2.1 Fréquence d'utilisation</h5>
+    <form action="">
+        <!-- Liste déroulante pour les Fréquence d'utilisation -->
+        <div>
+            <select id="frequence" name="frequence" onchange="checkAutre(this)">
+                <option value="" disabled selected hidden>Choisissez une fréquence d'utilisation</option>
+                <option value="Qutidienne">Qutidienne</option>
+                <option value="Hebdomadaire">Hebdomadaire</option>
+                <option value="Mensuelle">Mensuelle</option>
+                <option value="MoinsUneFois">Moins d'une fois par mois</option>
+            </select>
+        </div>
+    </form>
+    <h5>2.2 Principal motif d'utilisation</h5>
+    <form action="">
+        <!-- Liste déroulante pour le motif d'utilisation -->
+        <div>
+            <select id="motif" name="motif" onchange="checkAutre(this)">
+                <option value="" disabled selected hidden>Choisissez le principal motif d'utilisation</option>
+                <option value="Recherche">Recherche académique</option>
+                <option value="Personnel">Travail personnel</option>
+                <option value="Curiosite">Curiosité personnelle</option>
+                <option value="AutreMotif">Autre</option>
+            </select>
+        </div>
+    </form>
+    <h2>3. Facilité d'utilisation</h2>
+    <form action="" method="" id="">
+        @csrf
+        <!-- Case à cocher -->
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Très facile</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Facile</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Moyenne</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Difficile</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Très defficile</label>
+        </div>
+    </form>
+    <h2>4. Actualisation des informations</h2>
+    <form action="" method="" id="">
+        @csrf
+        <!-- Case à cocher -->
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Toujours à jour</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Souvent à jour</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Parfois à jour</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Rarement à jour</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Jamais à jour</label>
+        </div>
+    </form>
+    <h2>6. Satisfaction générales</h2>
+    <form action="" method="" id="">
+        @csrf
+        <!-- Case à cocher -->
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Très satisfait</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Satisfait</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Neutre</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Insatisfait</label>
+        </div>
+        <div class ="checkbox-container">
+            <input type="checkbox" id="agree" name="agree" value="1">
+            <label for="agree" style="margin-left: 8px;">Très insatisfait</label>
+        </div>
+    </form>
+    <h2>7. Suggestions pour amélioration</h2>
 <script src="{{ asset('js/script.js') }}"></script>
-
 </body>
 </html>
